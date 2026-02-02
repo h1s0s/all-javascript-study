94295e41e24b7224122e85f25944181378c03e91
h1s0s <hs94122@naver.com>
2026-02-02 13:14:02 +0900

    리액트공부 - 이벤트핸들링

--- react_in_bites_sized_cuts/react_app_1/src/components/Button.jsx
+++ react_in_bites_sized_cuts/react_app_1/src/components/Button.jsx
@@ -1,6 +1,15 @@
 const Button = ({ children, text, color = "black", ...rest}) => {//기본적으로 props, 객체형태로 들어오나 기본값이 필요할 때 구조분해할당을 해야함.
+    
     return (
-    <button style={{ color: color }} {...rest}>
+    <button 
+        onClick={()=>
+            console.log(text)
+        }
+        onMouseEnter={(e)=> {
+            console.log(e)
+            console.log("마우스를갖다댐")
+        }}
+        style={{ color: color }} {...rest}>
         {text} - {color?.toUpperCase()}
         {children}
     </button>)
