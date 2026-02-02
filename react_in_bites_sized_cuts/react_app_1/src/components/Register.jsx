8aebcb2b9410ce0aab7fec19d1525e77175ff1f4
h1s0s <hs94122@naver.com>
2026-02-02 17:08:42 +0900

    리액트공부 - State로 입력 관리하기(리팩토링)

--- react_in_bites_sized_cuts/react_app_1/src/components/Register.jsx
+++ react_in_bites_sized_cuts/react_app_1/src/components/Register.jsx
@@ -6,45 +6,40 @@ import {useState} from 'react'
 // 4. 자기소개
 
 const Register = () => {
-    const [name, setName] = useState("이름");
-    const [birth, setBirth] = useState("");
-    const [country, setCountry] = useState("");
-    const [bio, setBio] = useState("");
+    const [input, setInput] = useState({
+        name: "",
+        birth: "",
+        country: "",
+        bio: ""
+    });
 
-    const onChangeName = (e) => {
-        setName(e.target.value);
-    }
-
-    const onChangeBirth = (e) => {
-        setBirth(e.target.value)
-    }
-
-    const onChangeCountry = (e) => {
-        setCountry(e.target.value)
-    }
-
-    const onChangeBio = (e) => {
-        setBio(e.target.value)
+    const onChange = (e) => {
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value
+        })
     }
 
     return (
         <div>
             <div>
                 <input 
-                    value={name} 
-                    onChange={onChangeName} 
+                    name="name"
+                    value={input.name} 
+                    onChange={onChange} 
                     placeholder={"이름"}
                 />
             </div>
             <div>
                 <input
-                    value={birth}
+                    name="birth"
+                    value={input.birth}
                     type="date"
-                    onChange={onChangeBirth}
+                    onChange={onChange}
                 />
             </div>
             <div>
-                <select value={country} onChange={onChangeCountry}> 
+                <select name="country" value={input.country} onChange={onChange}> 
                     <option></option>
                     <option>한국</option>
                     <option>미국</option>
@@ -52,7 +47,7 @@ const Register = () => {
                 </select>
             </div>
             <div>
-                <textarea value={bio} onChange={onChangeBio}></textarea>
+                <textarea name="bio" value={input.bio} onChange={onChange}></textarea>
             </div>
         </div>
     )
