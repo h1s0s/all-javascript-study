931da29186e2259e006819183aa807442d422a9e
h1s0s <hs94122@naver.com>
2026-02-03 17:55:41 +0900

    리액트공부 - useEffect

--- react_in_bites_sized_cuts/react_count_app/src/App.jsx
+++ react_in_bites_sized_cuts/react_count_app/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import Viewer from './components/Viewer'
 import Controller from './components/Controller'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+  
+  useEffect(()=>{ //두번째 배열에 들어가있는 인수가 변경되면? 첫번째 인수의 콜백함수가 호출됨
+    console.log(`count: ${count} / input: ${input}`)
+  }, [count, input]);//의존성 배열, dependency array, deps
 
   const onClickButton = (value) => {
       setCount(count + value);
@@ -13,6 +18,11 @@ function App() {
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <section>
+        <input value={input} onChange={(e) => {
+          setInput(e.target.value);
+        }}/>
+      </section>
       <section>
         <Viewer count={count}/>
       </section>
